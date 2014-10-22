ed7dc5594f23365dca6f7b374508103c6254b018
ganger85 <ganger85@gmail.com>
2014-10-22 18:59:38 +0200

    add test standard decay

--- GildedRose/src/test/java/GildedRoseTest.java
+++ GildedRose/src/test/java/GildedRoseTest.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.ArrayList;
 
+import org.junit.Test;
 
 public class GildedRoseTest {
 
+	private static final int QUALITY_DECAY = 1;
+	private static final int ANY_QUALITY = 40;
+	private static final int ANY_SELLIN = 14;
+
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void qualityShouldDecay() {
+
+		Item product = new Item("ANY_NAME", ANY_SELLIN, ANY_QUALITY);
+		GildedRose.items = new ArrayList<Item>();
+		GildedRose.items.add(product);
+
+		GildedRose.updateQuality();
+
+		assertEquals("Quality should have decay", ANY_QUALITY - QUALITY_DECAY, product.quality);
 	}
 }
